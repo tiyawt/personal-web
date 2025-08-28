6e9c58d38ab532dfb813556b31a23fd674458da3
tiyawt <tiyaland3@gmail.com>
2025-08-28 15:26:48 +0700

    projects page

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-function luasLingkaran(r) {
-    const phi = 3.14
-    const luas = phi*r**2
-    return luas
-}
+/*
+Buat fungsi searchAndDouble(num) yang:
 
-console.log(luasLingkaran(4))
+Cek tipe data input, kalau bukan number return "Harus angka".
 
+Cari apakah num ada di array [2, 4, 6, 8, 10].
+
+Kalau ada, return num * 2. Kalau nggak, return false.
+
+*/
 
